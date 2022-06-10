786bbf597785378c656a299359e8d91cef395088
jonispahiu <104530585+jonispahiu@users.noreply.github.com>
2022-06-10 14:06:51 +0200

    first commit

---
+++ assignment.py
@@ -0,0 +1,29 @@
+for i in range (0, 151):
+    print (i)
+
+for i in range (5, 1001, 5):
+    print (i)
+
+for i in range (0, 101):
+    if i %10 == 0:
+        print ("codingdojo")
+    elif i %5 == 0:
+        print ("coding")
+    else:
+        print (i)
+
+sum= 0
+for i in range (1,500001,2):
+    sum= sum + i
+print (sum)
+
+for i in range (2018, 0, -4):
+    print (i)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range (lowNum, highNum + 1):
+    if i %mult ==0:
+        print (i)
+
